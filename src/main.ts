1244712840efcfe079354f72500b7d5344df693c
stbearangel <stbearangel@sina.com>
2025-06-12 12:27:34 +0800

    新增swagger支持

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigUtil } from './config/config.util';
+import { SwaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,9 @@ async function bootstrap() {
 
   const port = ConfigUtil.getNumber('app.server.port', 3000);
   const appName: string = ConfigUtil.getString('app.server.name', 'appName');
+
+  // 设置Swagger
+  SwaggerConfig.setup(app);
 
   await app.listen(port);
   logger.log(`应用 "${appName}" 已启动，监听端口: ${port}`);
